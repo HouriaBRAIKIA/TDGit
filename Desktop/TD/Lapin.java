7d0629694af332c77e55bd7a794da038189b2faa
Houria BRAIKIA <83876346+HouriaBRAIKIA@users.noreply.github.com>
2023-11-08 20:14:44 +0100

    trois version

---
+++ Desktop/TD/Lapin.java
@@ -0,0 +1,9 @@
+
+//Dans Lapin.java
+public class Lapin extends Animal{
+    @Override
+     void seDeplacer(int x, int y){ //erreur public
+        super.seDeplacer(x,y)
+        System.out.println("Lapin !")
+    }
+}
