f31b69bedc605144f26cc67a1460bf2771eba24b
Houria BRAIKIA <83876346+HouriaBRAIKIA@users.noreply.github.com>
2023-11-08 20:24:07 +0100

    correction err

--- Desktop/TD/Lapin.java
+++ Desktop/TD/Lapin.java
@@ -2,7 +2,7 @@
 //Dans Lapin.java
 public class Lapin extends Animal{
     @Override
-     void public seDeplacer(int x, int y){ //erreur public
+     void  seDeplacer(int x, int y){ //erreur public
         super.seDeplacer(x,y)
         System.out.println("Lapin !")
     }
